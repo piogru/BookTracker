82897214b955935478a3c849b22c3fa3831a68ad
Piotr G <81086063+piogru@users.noreply.github.com>
2022-01-01 18:35:31 +0100

    Add current month's statistics

--- app/src/main/java/com/example/booktracker/StatisticsFragment.java
+++ app/src/main/java/com/example/booktracker/StatisticsFragment.java
@@ -14,7 +14,11 @@ import android.widget.TextView;
 import com.example.booktracker.database.BookViewModel;
 import com.example.booktracker.database.entities.BookWithAuthors;
 
+import java.time.LocalDate;
+import java.time.Month;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 public class StatisticsFragment extends Fragment {
@@ -26,13 +30,30 @@ public class StatisticsFragment extends Fragment {
     private int totalBooksFinished;
     private int totalAverageTime;
     private int totalTimeSpent;
+    private int totalPagesRead;
 
-    private List<Long> bookTimes;
+    private int monthlyBooksStarted;
+    private int monthlyBooksFinished;
+    private int monthlyAverageTime;
+    private int monthlyTimeSpent;
+    private int monthlyPagesRead;
+
+    private List<Long> totalBookTimes;
+    private List<Long> monthlyBookTimes;
+
+    private List<BookWithAuthors> monthlyBooks;
 
     private TextView totalBooksStartedTextView;
     private TextView totalBooksFinishedTextView;
     private TextView totalAverageTimeTextView;
     private TextView totalTimeSpentTextView;
+    private TextView totalPagesReadTextView;
+
+    private TextView monthlyBooksStartedTextView;
+    private TextView monthlyBooksFinishedTextView;
+    private TextView monthlyAverageTimeTextView;
+    private TextView monthlyTimeSpentTextView;
+    private TextView monthlyPagesReadTextView;
 
     public StatisticsFragment() {
         // Required empty public constructor
@@ -40,7 +61,15 @@ public class StatisticsFragment extends Fragment {
         totalBooksFinished = 0;
         totalAverageTime = 0;
         totalTimeSpent = 0;
-        bookTimes = new ArrayList<>();
+        totalBookTimes = new ArrayList<>();
+
+        monthlyBooksStarted = 0;
+        monthlyBooksFinished = 0;
+        monthlyAverageTime = 0;
+        monthlyTimeSpent = 0;
+        monthlyBookTimes = new ArrayList<>();
+
+        monthlyBooks = new ArrayList<>();
     }
 
     public static StatisticsFragment newInstance() {
@@ -76,36 +105,75 @@ public class StatisticsFragment extends Fragment {
             @Override
             public void onChanged(List<BookWithAuthors> select) {
                 books = select;
+                Date currentDate = new Date();
+                Calendar cal = Calendar.getInstance();
+                cal.setTime(currentDate);
+                int month = cal.get(Calendar.MONTH);
+                int bookMonth;
 
+                // total statistics
                 totalBooksStarted = books.size();
 
                 for(BookWithAuthors book : books) {
                     if(book.book.getEndDate() != null) {
                         totalBooksFinished += 1;
                         totalTimeSpent += book.book.getTimeSpent();
+                        totalPagesRead += book.book.getPageCount();
 
                         long time = book.book.getEndDate().getTime() - book.book.getStartDate().getTime();
-                        bookTimes.add(time);
+                        time = time / 1000 / 60; // time in minutes
+                        totalBookTimes.add(time);
+
+                        cal.setTime(book.book.getEndDate());
+                        bookMonth = cal.get(Calendar.MONTH);
+                        if(month == bookMonth) {
+                            monthlyBooks.add(book);
+                        }
                     }
-
-
-//            bookTime.add(Long.valueOf(time));
                 }
-                totalAverageTime = calculateAverage(bookTimes);
+                totalAverageTime = calculateAverage(totalBookTimes);
 
                 totalBooksStartedTextView = view.findViewById(R.id.statistics_total_books_started);
                 totalBooksFinishedTextView = view.findViewById(R.id.statistics_total_books_finished);
                 totalAverageTimeTextView = view.findViewById(R.id.statistics_total_average_time);
                 totalTimeSpentTextView = view.findViewById(R.id.statistics_total_time_spent);
+                totalPagesReadTextView = view.findViewById(R.id.statistics_total_pages_read);
 
                 totalBooksStartedTextView.setText(String.valueOf(totalBooksStarted));
                 totalBooksFinishedTextView.setText(String.valueOf(totalBooksFinished));
                 totalAverageTimeTextView.setText(String.valueOf(totalAverageTime));
                 totalTimeSpentTextView.setText(String.valueOf(totalTimeSpent));
+                totalPagesReadTextView.setText(String.valueOf(totalPagesRead));
+
+                // monthly statistics
+                monthlyBooksStarted = monthlyBooks.size();
+
+                for(BookWithAuthors book : monthlyBooks) {
+                    if(book.book.getEndDate() != null) {
+                        monthlyBooksFinished += 1;
+                        monthlyTimeSpent += book.book.getTimeSpent();
+                        monthlyPagesRead += book.book.getPageCount();
+
+                        long time = book.book.getEndDate().getTime() - book.book.getStartDate().getTime();
+                        time = time / 1000 / 60; // time in minutes
+                        monthlyBookTimes.add(time);
+                    }
+                }
+                monthlyAverageTime = calculateAverage(monthlyBookTimes);
+
+                monthlyBooksStartedTextView = view.findViewById(R.id.statistics_monthly_books_started);
+                monthlyBooksFinishedTextView = view.findViewById(R.id.statistics_monthly_books_finished);
+                monthlyAverageTimeTextView = view.findViewById(R.id.statistics_monthly_average_time);
+                monthlyTimeSpentTextView = view.findViewById(R.id.statistics_monthly_time_spent);
+                monthlyPagesReadTextView = view.findViewById(R.id.statistics_monthly_pages_read);
+
+                monthlyBooksStartedTextView.setText(String.valueOf(monthlyBooksStarted));
+                monthlyBooksFinishedTextView.setText(String.valueOf(monthlyBooksFinished));
+                monthlyAverageTimeTextView.setText(String.valueOf(monthlyAverageTime));
+                monthlyTimeSpentTextView.setText(String.valueOf(monthlyTimeSpent));
+                monthlyPagesReadTextView.setText(String.valueOf(monthlyPagesRead));
             }
         });
-
-
 
         return view;
     }
