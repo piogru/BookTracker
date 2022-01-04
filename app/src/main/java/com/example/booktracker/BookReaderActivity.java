5db4201bc0e9923bce925dac871057bfff35b71b
piogru <81086063+piogru@users.noreply.github.com>
2022-01-04 22:43:45 +0100

    Add rotation handling in activities and fragments (#11)
    * Add rotation handling in BooksFragment and MainActivity
    
    * Add rotation handling to StatisticsFragment
    
    * Add rotation handling in BookSearchFragment
    
    * Handle rotation in book reader and add currentPage attributeto Book

--- app/src/main/java/com/example/booktracker/BookReaderActivity.java
+++ app/src/main/java/com/example/booktracker/BookReaderActivity.java
@@ -30,8 +30,12 @@ import java.io.File;
 public class BookReaderActivity extends AppCompatActivity {
 
     public final static String EXTRA_FILE_URI = "EXTRA_FILE_URI";
+    public final static String EXTRA_CURRENT_PAGE= "EXTRA_CURRENT_PAGE";
+
+    public final static String KEY_CURRENT_PAGE = "current_page";
 
     private PDFView pdfView;
+    private int currentPage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,9 +50,13 @@ public class BookReaderActivity extends AppCompatActivity {
         Uri uri = Uri.parse(fileUri);
         File file = new File(uri.getPath());
 
-//        pdfView.getCurrentPage();
+        if(savedInstanceState != null) {
+            currentPage = savedInstanceState.getInt(KEY_CURRENT_PAGE);
+        } else {
+            currentPage = intent.getIntExtra(EXTRA_CURRENT_PAGE, 0);
+        }
 
-        pdfView.fromUri(uri).defaultPage(1).onLoad(new OnLoadCompleteListener() {
+        pdfView.fromUri(uri).defaultPage(currentPage).onLoad(new OnLoadCompleteListener() {
             @Override
             public void loadComplete(int nbPages) {
                 Toast.makeText(BookReaderActivity.this, String.valueOf(nbPages), Toast.LENGTH_LONG).show();
@@ -56,18 +64,15 @@ public class BookReaderActivity extends AppCompatActivity {
         }).load();
 
         createNotificationChannel();
-
         Resources resources = getResources();
-
         Intent notificationIntent = new Intent(this, BookReaderActivity.class);
         notificationIntent.setAction(Intent.ACTION_MAIN);
         notificationIntent.addCategory(Intent.CATEGORY_LAUNCHER);
-//        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         intent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
         notificationIntent.putExtra(EXTRA_FILE_URI, intent.getStringExtra(EXTRA_FILE_URI));
         PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
 
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "READER_CHANNEL")
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "BOOK_READER_CHANNEL")
                 .setSmallIcon(R.drawable.ic_book_black_24dp)
                 .setContentTitle(resources.getString(R.string.app_name))
                 .setContentText(resources.getString(R.string.notification_text) + " " +
@@ -103,8 +108,9 @@ public class BookReaderActivity extends AppCompatActivity {
             CharSequence name = getString(R.string.channel_name);
             String description = getString(R.string.channel_description);
             int importance = NotificationManager.IMPORTANCE_DEFAULT;
-            NotificationChannel channel = new NotificationChannel("READER_CHANNEL", name, importance);
+            NotificationChannel channel = new NotificationChannel("BOOK_READER_CHANNEL", name, importance);
             channel.setDescription(description);
+            channel.setSound(null, null);
             // Register the channel with the system; you can't change the importance
             // or other notification behaviors after this
             NotificationManager notificationManager = getSystemService(NotificationManager.class);
@@ -116,8 +122,16 @@ public class BookReaderActivity extends AppCompatActivity {
     @Override
     public void finish() {
         Intent replyIntent = new Intent();
+        replyIntent.putExtra(EXTRA_CURRENT_PAGE, pdfView.getCurrentPage());
         setResult(300, replyIntent);
 
         super.finish();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        outState.putInt(KEY_CURRENT_PAGE, pdfView.getCurrentPage());
     }
 }
